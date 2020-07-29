4fe6dfebd7b0b9bc75d9e6e0b01aacc5e1d36eb9
AshishSharman <68974976+AshishSharman@users.noreply.github.com>
2020-07-29 19:25:51 -0400

    Add files via upload

---
+++ assets/data/myCars.ts
@@ -0,0 +1,28 @@
+import { carDetails } from '../../app/carInfo';
+
+export const MYCLASSES: carDetails[] = [
+    {make: 'BMW',
+     model: 'X1',
+     body: 'SUV',
+     color: 'White',
+     picture: 'assets/images/1.jpg', 
+     },
+     {make: 'Nissan',
+     model: 'GT-R',
+     body: 'Sports',
+     color: 'Blue',
+     picture: 'assets/images/2.jpg', 
+     },
+     {make: 'BMW',
+     model: 'Beamer',
+     body: 'Sports',
+     color: 'Black',
+     picture: 'assets/images/3.jpg', 
+     },
+     {make: 'Mercdez-Benz',
+     model: 'Silver arrow ',
+     body: 'Supercar',
+     color: 'Metallic gray',
+     picture: 'assets/images/4.jpg', 
+     }     
+    ];
